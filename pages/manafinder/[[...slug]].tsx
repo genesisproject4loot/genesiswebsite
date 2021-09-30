b5af8249ab664a8a616ba3b67ebca91ba71f6e0f
tenoxious <greg.choi+oxten@gmail.com>
2021-09-30 08:09:12 -0700

    Pull out graphql code from slug file

--- pages/manafinder/[[...slug]].tsx
+++ pages/manafinder/[[...slug]].tsx
@@ -1,12 +1,13 @@
 // Imports
 import Layout from "@components/Layout"; // Layout wrapper
 import styles from "@styles/pages/Manafinder.module.scss"; // Styles
-import { gql,useQuery } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 import { useRouter } from "next/router";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo } from "react";
 import Select from 'react-select'
 import suffices from '@data/suffices.json'
 import inventory from '@data/inventory.json'
+import {useUnclaimedMana, useClaimedMana} from 'hooks/useMana';
 
 // Types
 import type { ReactElement } from "react";
@@ -117,62 +118,6 @@ function UnClaimedMana(props: {suffixId:number, inventoryId:number}) {
 }
 
 
-function useClaimedMana(suffixId, inventoryId) {
-
-  const GET_CLAIMED_MANA = gql`
-  query GetClaimedMana($suffixId: String!, $inventoryId: Int!) {
-    manas(where: {suffixId: $suffixId, inventoryId: $inventoryId, currentOwner_not_in:
-        ["0x1884d71487bfd7f595061221801e783efcd0bf6a",
-         "0x9bbda2777c8623d8894b21120bed1fff72b024f8",
-         "0xb6b3eb3ec30bd8979df60d7f47b173a389310dd9",
-         "0xc3e33b881aea922bce6df56bc2c6f0686a3a421a",
-         "0xdcab536df6dd9ad5d332180edf1ba1ec71669ae2",
-         "0x4cf7e239f5bc882e007d9790a7b49b4abdfeb510",
-         "0x338aef050ac689246490aa75b691ac03fe0a81c8",
-         "0xea0a3aae1dda17d0a17a488196801f59ca96854c",
-         "0x1f5fe23574d5aec1660a8c6b209135da5723042f",
-         "0xd431a116a7d0eca9371614d5652cdfffb7c5b6eb",
-         "0x0780529f0ecfac1048d2faae5007fe62ce318c79",
-         "0xf935c944a8e03181ae3229774d4b93d7bba816d4",
-         "0xaba619a78032abcfe0346c1592835df563ba3bfa",
-         "0xe1e5072fc1ff1a419d17bf9b5c5b6ddd12c19f08",
-         "0x17498d27433849a510165cc1fc618582ac54229b",
-         "0xb1dea25cb8b997913f86076b372aa75f06c53c99"]}) {
-      id
-      itemName
-      currentOwner {
-        id
-      }
-    }
-  }
-  `;
-  return useQuery<ManaData, ManaVars>(
-    GET_CLAIMED_MANA,
-    { variables: { suffixId: String(suffixId), inventoryId: inventoryId } }
-  );
-}
-
-function useUnclaimedMana(suffixId, inventoryId) {
-  const inventoryName = (inventoryId >= 0) ? inventory[inventoryId].label.toLowerCase() : 'weapon';
-  const inventoryNameSuffix = inventoryName + "SuffixId";
-
-  const GET_UNCLAIMED_MANA = gql`
-  query GetUnclaimedMana($suffixId: Int!) {
-    bags(where: {manasClaimed: 0, ${inventoryNameSuffix}: $suffixId}) {
-      id
-      itemName: ${inventoryName}
-      currentOwner {
-        id
-      }
-    }
-  }
-  `;
-  
-  return useQuery<BagData, BagVars>(
-    GET_UNCLAIMED_MANA,
-    { variables: { suffixId: suffixId } }
-  );
-}
 
 function useOpenseaManaData(tokenIds:string[]) {
   const GET_OPENSEA_MANA_DATA = gql`
