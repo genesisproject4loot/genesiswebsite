c4c4038ca86024751f62650cbfe6454f6e742587
tenoxious <greg.choi+oxten@gmail.com>
2021-10-04 01:09:39 -0700

    Add wallet support

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"; // Routing
+import { useRouter } from "next/router"; // Routing
+import styles from "@styles/components/Layout.module.scss"; // Styles
+import { useWalletContext } from "hooks/useWalletContext";
+
+export default function Header() {
+  const { connectWallet, isConnected, disconnectWallet, displayName } =
+    useWalletContext();
+
+  return (
+    <div className={styles.header}>
+      {/* Main logo */}
+      <div className={styles.header__logo}>
+        <Link href="/">
+          <a>
+            <img src="/genesis-icon.png" height="35px"></img> Genesis Project
+            (for Loot)
+          </a>
+        </Link>
+      </div>
+      <div className={styles.header__account}>
+        {isConnected && (
+          <span>
+            {displayName} {" "}
+            <a
+              className={styles.header__links_active}
+              onClick={disconnectWallet}
+            >
+              [ disconnect ]
+            </a>
+          </span>
+        )}
+        {!isConnected && (
+          <button className={styles.header__button} onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
