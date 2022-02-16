d43e867fa2c9585e789c437fdb2072e46e473783
tenoxious <greg.choi+oxten@gmail.com>
2022-02-15 21:45:07 -0800

    Add rank filter. Remove sort.

--- utils/constants.ts
+++ utils/constants.ts
@@ -105,4 +105,27 @@ export const ITEM_GREATNESS = [
   }
 ];
 
+export const ITEM_RANK = [
+  {
+    value: "1",
+    label: "1"
+  },
+  {
+    value: "2",
+    label: "2"
+  },
+  {
+    value: "3",
+    label: "3"
+  },
+  {
+    value: "4",
+    label: "4"
+  },
+  {
+    value: "5",
+    label: "5"
+  }
+];
+
 export { SUFFICES, INVENTORY };
